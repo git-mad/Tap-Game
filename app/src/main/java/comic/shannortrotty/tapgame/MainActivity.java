cf4dc0f295bc9c0c23f4b03d571086cad927d624
Jamal Paden <j.paden12@yahoo.com>
2019-11-17 21:55:01 -0500

    Updating settings in app

--- app/src/main/java/comic/shannortrotty/tapgame/MainActivity.java
+++ app/src/main/java/comic/shannortrotty/tapgame/MainActivity.java
@@ -20,14 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        playerOneScore = ((TextView) findViewById(R.id.textView_main_activity_player1_score));
-        playerTwoScore = ((TextView) findViewById(R.id.textView_main_activity_player2_score));
-        playerOneButton = ((Button) findViewById(R.id.btn_main_activity_player1));
-        playerTwoButton = ((Button) findViewById(R.id.btn_main_activity_player2));
+        playerOneScore = findViewById(R.id.textView_main_activity_player1_score);
+        playerTwoScore = findViewById(R.id.textView_main_activity_player2_score);
+        playerOneButton = findViewById(R.id.btn_main_activity_player1);
+        playerTwoButton = findViewById(R.id.btn_main_activity_player2);
 
 
-        playerOneScore.setText((String.valueOf("0")));
-        playerTwoScore.setText(String.valueOf("0"));
+        playerOneScore.setText("0");
+        playerTwoScore.setText("0");
 
         playerOneButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
                 value++;
                 if( value >= WINNING_SCORE){
                     Toast.makeText(MainActivity.this, "Player one Wins!", Toast.LENGTH_SHORT).show();
-                    playerOneScore.setText((String.valueOf("0")));
-                    playerTwoScore.setText(String.valueOf("0"));
+                    playerOneScore.setText("0");
+                    playerTwoScore.setText("0");
                 }else{
                     playerOneScore.setText(String.valueOf(value));
                 }
@@ -51,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
                 value++;
                 if (value >= WINNING_SCORE){
                     Toast.makeText(MainActivity.this, "Player Two Wins!", Toast.LENGTH_SHORT).show();
-                    playerOneScore.setText((String.valueOf("0")));
-                    playerTwoScore.setText(String.valueOf("0"));
+                    playerOneScore.setText("0");
+                    playerTwoScore.setText("0");
                 }else{
                     playerTwoScore.setText(String.valueOf(value));
                 }
